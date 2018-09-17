b1430a383343d3d11562dd8be774c3390d5dcdc2
@boodah <boodah21@protonmail.com>
2018-09-17 13:02:22 -0500

    Se Crea la clase para convertir de Pascal a C#

---
+++ repos/Airport_convert_Pascal_to_C#/Airport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Airport
+{
+    int[] vector = new int[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    vector v;
+    int n, i, a, p;
+    String res, c;
+    char opc;
+    public Airport()
+    {
+    }//end builder
+
+     public String verificar_puesto(vector v, int puesto)
+    {
+        if (v[puesto] == 1)
+        {
+            return "OCUPADO";
+        }
+        else { return "DESOCUPADO"; }
+    }//end verificar_puesto
+
+    public int impresion(vector v)
+    {
+        int x, y;
+        Console.Clear();
+        Console.WriteLine();
+        Console.WriteLine("\t\tIMPRESION DE RESERVA AEREA NO FUMADOR");
+        Console.WriteLine("\t\t\t\tPUESTOS DEL 1/5")
+    }
+}//end class
