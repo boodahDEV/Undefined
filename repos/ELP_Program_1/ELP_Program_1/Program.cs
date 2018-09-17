a321506da779d0eba2a73c5e8bf2f4fc113a5bff
boodah <boodah21@protonmail.com>
2018-09-17 12:57:51 -0500

    Se alojan proyectos de Estructura de los LP

---
+++ repos/ELP_Program_1/ELP_Program_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ELP_Program_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+          
+                Console.WriteLine("\t\t TABLA DE MULTIPLICAR DE X NUMERO");
+                Console.Write("\n Inserte el numero que decea conocer: ");
+                n = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("\n\n");
+                for (int i = 0; i <= 12; i++)
+                {
+                    Console.WriteLine(n + " * " + i + " = " + (n * i));
+                }
+            Console.Read();
+
+        }
+    }
+}
